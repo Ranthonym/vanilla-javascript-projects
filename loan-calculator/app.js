6e880d1e39e85b0222f2fe0bbd64d452c91a97ca
mithrax <rahul.anthonym@gmail.com>
2020-05-04 12:29:23 -0400

    add event listeners, monthly. total payment and total interest calculations + error msg

--- loan-calculator/app.js
+++ loan-calculator/app.js
@@ -1,8 +1,3 @@
-// listen for submit
-document
-  .getElementById("loand-form")
-  .addEventListener("submit", calculateResults);
-
 // calculate results
 const calculateResults = (e) => {
   // UI variables
@@ -17,5 +12,42 @@ const calculateResults = (e) => {
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
+  //compute monthly payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    showError("Please check your numbers");
+  }
+
   e.preventDefault();
 };
+
+// show error
+const showError = (error) => {
+  //create div
+  const errorDiv = document.createElement("div");
+  // get elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+  // add class
+  errorDiv.className = "alert alert-danger";
+  // create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+  // insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // clear error msg after 3 seconds
+  setTimeout(() => {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
+
+// listen for submit
+document
+  .getElementById("loan-form")
+  .addEventListener("submit", calculateResults);
